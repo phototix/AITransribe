0719aaebe18872708d372f8b87528c4a35c3507e
Amanda <phototix@hotmail.com>
2025-08-08 23:36:14 +0800

    Merge branch 'main' of https://github.com/phototix/task-manager

--- api/translate.php
+++ api/translate.php
@@ -68,7 +68,7 @@ try {
 }
 
 function translateWithOpenAI($api_key, $model, $text, $source_lang, $target_lang) {
-    $prompt = "Translate the following text from {$source_lang} to {$target_lang}:\n\n{$text}";
+    $prompt = "Translate the following text from {$source_lang} to {$target_lang}:\n\n{$text}. Note: If both same, just output the source text instead.";
     
     $response = openaiRequest($api_key, $model, $prompt);
     
